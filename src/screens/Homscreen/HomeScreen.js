08b9b58d8f3632c41c47b939595d5d5bd1b1fcb3
nguyenhue <nguyenhue211032@gmail.com>
2021-01-13 13:34:45 +0700

    innital

---
+++ src/screens/Homscreen/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Image,
+} from 'react-native';
+
+import styles from './styles'
+ import Profile from '../Profile/Profile'
+const HomeScreen = () => {
+  return(<View>
+
+<Profile/>
+    </View>);
+  
+};
+export default HomeScreen;
---
+++ src/screens/Homscreen/styles.js
@@ -0,0 +1,21 @@
+import {StyleSheet} from 'react-native';
+const styles = StyleSheet.create({
+  container: {
+   // padding: 10,
+    width: 86,
+    height: 86,
+    borderRadius:40,
+    borderWidth: 3,
+    borderColor: 'red',
+    margin:10
+  },
+
+  viewImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    borderWidth: 1,
+    borderColor: 'white',
+  },
+});
+export default styles;
